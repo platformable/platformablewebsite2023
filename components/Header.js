83708cfb68637bf2d0684ac2c62fca1aee85ffc3
Mayadev3 <107764065+Mayadev3@users.noreply.github.com>
2023-06-22 11:18:54 +0200

    testing new branch

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,5 @@
-import React from 'react'
+import React from "react";
 
 export default function Header() {
-  return (
-    <div>Header Component</div>
-  )
+  return <div>Header Component ONE</div>;
 }
