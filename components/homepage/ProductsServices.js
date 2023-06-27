92a1292ff3955f0401fec8f769e83796b3e2458e
Alexei Garban <garban.valdeon@gmail.com>
2023-06-27 15:42:38 +0200

    sections added

---
+++ components/homepage/ProductsServices.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ProductsServices() {
+  return (
+    <div>ProductsServices</div>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,7 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import Layout from '../../components/Layout'
+import styles from '@/styles/Homepage.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -7,7 +9,7 @@ export default function Home() {
   return (
 
     <Layout>
- 
+ <h3>platformable</h3>
     </Layout>
   )
 }
