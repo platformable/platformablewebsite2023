6c147a21f83a426353c95f9adedba201b1fc3370
leoncan122 <leon.can122@gmail.com>
2023-06-27 18:34:48 +0200

    Add support ecosystem seciton

--- components/ListItemCard.js
+++ components/ListItemCard.js
@@ -1,7 +1,10 @@
 import React from 'react'
 
-export default function ListItemCard() {
+export default function ListItemCard({img_src, img_alt, item_description}) {
   return (
-    <div>ListItemCard</div>
+    <div className='bg-white rounded-xl shadow-md p-4 flex gap-5'>
+        <img src={img_src} alt={img_alt} />
+        <p className='font-medium'>lorem</p>
+    </div>
   )
 }
--- components/homepage/SupportOpenEcosystems.js
+++ components/homepage/SupportOpenEcosystems.js
@@ -1,7 +1,25 @@
 import React from 'react'
+import ListItemCard from '../ListItemCard'
 
 export default function SupportOpenEcosystems() {
+  const support_open_ecosystems_title = 'How Platformable supports open ecosystems'
+  const items = ['1', '2', '3','4', '5', '6', '7', '8']
   return (
-    <div>SupportOpenEcosystems</div>
+    <section className='bg-green-medium '>
+      <div className='flex gap-10  mb-10'>
+        <img src='' alt="plat logo" width={100}/>
+        <h2 className='text-[#2A2FC1] font-bold font-karla '>{support_open_ecosystems_title}</h2>
+
+      </div>
+      <div className='px-32'>
+        <div className='grid grid-rows-4 grid-cols-2 xl:gap-y-5 xl:gap-8'>
+          {
+            items.map((item, i) => (
+              <ListItemCard item_description={item} key={i}/>
+            ))
+          }
+        </div>
+      </div>
+    </section>
   )
 }
