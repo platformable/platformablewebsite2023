a3498bc6de0b5c7b88aefda13a5a1b79026518b0
Alexei Garban <garban.valdeon@gmail.com>
2023-06-30 20:59:49 +0200

    update testimonials cards

--- components/homepage/TestimonialCard.js
+++ components/homepage/TestimonialCard.js
@@ -2,36 +2,49 @@ import React from "react";
 
 export default function TestimonialCard({ testimonial, index }) {
   return (
-    <div className="rounded-lg bg-white shadow-md relative pb-36">
-      <div className="items-center p-7">
-        <div>
+    <>
+
+<div className="item bg-white rounded-tl-lg rounded-tr-lg relative">
+              <img src={testimonial.testimonials_img.data.attributes.url}
+            alt="Logo" />
+             <div
+            dangerouslySetInnerHTML={{ __html: testimonial.testimonials_text }}
+            className="py-7 italic font-light"
+          />
+
+          <div className="absolute bottom-0 left-0 right-0 bg-green-dark rounded-tl-lg rounded-tr-lg p-7">
+             <p>Melissa</p>
+             <span>CEO</span>
+            </div>
+            </div>
+
+            
+    {/*   <div className="item bg-white rounded-tl-lg rounded-tr-lg relative">
+        <div className="p-7">
           <img
-            className="mb-20"
+            className="mb-10"
             src={testimonial.testimonials_img.data.attributes.url}
             alt="Logo"
             width={200}
             height={40}
           />
-          {/* <img src="/logo.png" alt="" /> */}
+          <div
+            dangerouslySetInnerHTML={{ __html: testimonial.testimonials_text }}
+            className="py-7 italic font-light"
+          />
         </div>
-        <div
-          dangerouslySetInnerHTML={{ __html: testimonial.testimonials_text }}
-          className="py-7 italic font-light"
-        />
-      </div>
+        <div className="bg-green-dark rounded-tl-lg rounded-tr-lg p-7">
+          <p className={`${index % 2 == 0 ? "text-white" : "text-black"}`}>
+            {testimonial.testimonials_clientname}
+          </p>
+          <span className={`${index % 2 == 0 ? "text-white" : "text-black"}`}>
+            {testimonial.testimonials_client_role}
+          </span>
+        </div>
+      </div> */}
 
-      <div
-        className={`${
-          index % 2 == 0 ? "bg-blue-medium" : "bg-green-dark"
-        } absolute bottom-0 left-0 right-0  p-10 rounded-bl-lg rounded-br-lg`}
-      >
-        <p className={`${index % 2 == 0 ? "text-white" : "text-black"}`}>
-          {testimonial.testimonials_clientname}
-        </p>
-        <span className={`${index % 2 == 0 ? "text-white" : "text-black"}`}>
-          {testimonial.testimonials_client_role}
-        </span>
-      </div>
-    </div>
+      {/* ////// */}
+     
+    </>
   );
 }
--- components/homepage/Testimonials.js
+++ components/homepage/Testimonials.js
@@ -3,51 +3,119 @@ import TestimonialCard from "./TestimonialCard";
 import styles from "@/styles/Homepage.module.css";
 
 export default function Testimonials({ data, title }) {
-
-
-  const logos =[
+  const logos = [
     {
-      logo:'/LN_logo.png'
+      logo: "/LN_logo.png",
     },
     {
-      logo:'/iseal_logo.png'
+      logo: "/iseal_logo.png",
     },
     {
-      logo:'/axway_logo.png'
+      logo: "/axway_logo.png",
     },
     {
-      logo:'/APIToS_logo.png'
+      logo: "/APIToS_logo.png",
     },
     {
-      logo:'/odi_logo.png'
+      logo: "/odi_logo.png",
     },
     {
-      logo:'/alias_logo.png'
+      logo: "/alias_logo.png",
     },
-  ]
+  ];
   return (
     <section className={`pt-40 pb-10 ${styles["testimonials-bg"]}`}>
       <div className="flex gap-10  mb-10">
-        <img src="/icon_section05.svg"  alt="platformable logo" className='h-20 -ml-48 lg:-ml-36' />
+        <img
+          src="/icon_section05.svg"
+          alt="platformable logo"
+          className="h-20 -ml-48 lg:-ml-36"
+        />
         <h2 className="text-[#5B24EC] font-bold font-karla">{title}</h2>
       </div>
-      <div className="px-5 lg:px-32 ">
-        <div className="grid md:grid-cols-2 grid-cols-1  md:gap-y-0 gap-y-5 gap-x-10 md:my-0 md:px-32 px-5">
-          {data && data?.map((testimonial,index)=>{
-            return (
-              <div key={index}>
-            <TestimonialCard testimonial={testimonial}  index={index}/>
+      <div className="container mx-auto">
+        <div className="grid md:grid-cols-[1fr_1fr] gap-x-10 lg:px-32">
+          {data &&
+            data.map((testimonial, index) => {
+              return (
+                <div
+                  className="item bg-white rounded-tl-lg rounded-tr-lg relative p-7"
+                  key={index}
+                >
+                  <img
+                    src={testimonial.testimonials_img.data.attributes.url}
+                    alt=""
+                  />
+                  <div
+                    dangerouslySetInnerHTML={{
+                      __html: testimonial.testimonials_text,
+                    }}
+                    className="py-40 italic font-light"
+                  />
+
+                  <div
+                    className={`${
+                      index % 2 == 0 ? "bg-blue-medium" : "bg-green-dark "
+                    } absolute bottom-0 left-0 right-0 rounded-tl-lg rounded-tr-lg p-7`}
+                  >
+                    <p
+                      className={`${
+                        index % 2 == 0 ? "text-white" : "text-black"
+                      }`}
+                    >
+                      {testimonial.testimonials_clientname}
+                    </p>
+                    <span
+                      className={`${
+                        index % 2 == 0 ? "text-white" : "text-black"
+                      }`}
+                    >
+                      {testimonial.testimonials_client_role}
+                    </span>
+                  </div>
+                </div>
+              );
+            })}
+
+          {/* <div className="item bg-white rounded-tl-lg rounded-tr-lg relative">
+              <img src="https://dummyimage.com/120x50/000/fff" alt="" />
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, eaque deserunt? Quasi reprehenderit illum itaque consectetur facilis repellendus architecto impedit nostrum, similique vitae sint facere earum dicta nulla debitis nam.
+               
+              </p>
+
+          <div className="absolute bottom-0 left-0 right-0 bg-green-dark rounded-tl-lg rounded-tr-lg p-7">
+             <p>Melissa</p>
+             <span>CEO</span>
             </div>
-            )
-          })}
- 
+            </div> */}
+
+          {/*    <div className="item bg-white rounded-tl-lg rounded-tr-lg relative">
+              <img src="https://dummyimage.com/120x50/000/fff" alt="" />
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, eaque deserunt? Quasi reprehenderit illum itaque consectetur facilis repellendus architecto impedit nostrum, similique vitae sint facere earum dicta nulla debitis nam.
+
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, eaque deserunt? Quasi reprehenderit illum itaque consectetur facilis repellendus architecto impedit nostrum, similique vitae sint facere earum dicta nulla debitis nam.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, eaque deserunt? Quasi reprehenderit illum itaque consectetur facilis repellendus architecto impedit nostrum, similique vitae sint facere earum dicta nulla debitis nam.
+               
+              </p>
+
+          <div className="absolute bottom-0 left-0 right-0 bg-green-dark rounded-tl-lg rounded-tr-lg p-7">
+             <p>Melissa</p>
+             <span>CEO</span>
+            </div>
+            </div> */}
         </div>
+
         <div className="grid gap-x-5 md:grid-cols-6 grid-cols-2 md:gap-y-0 gap-y-3 items-center mt-20 justify-center">
-        {logos && logos.map((logo,index)=>{
-          return (
-            <div key={index}><img src={logo.logo} alt={'platformable'}/></div>
-          )
-        })}
+          {logos &&
+            logos.map((logo, index) => {
+              return (
+                <div key={index}>
+                  <img src={logo.logo} alt={"platformable"} />
+                </div>
+              );
+            })}
         </div>
       </div>
     </section>
