4ed9eda09e0156b0a9fb0039f48d8adfb9710f9e
Alexei Garban <garban.valdeon@gmail.com>
2023-06-30 18:18:26 +0200

    sections update on homepage

--- components/homepage/TestimonialCard.js
+++ components/homepage/TestimonialCard.js
@@ -1,7 +1,37 @@
-import React from 'react'
+import React from "react";
 
-export default function TestimonialCard() {
+export default function TestimonialCard({ testimonial, index }) {
   return (
-    <div>TestimonialCard</div>
-  )
+    <div className="rounded-lg bg-white shadow-md relative pb-36">
+      <div className="items-center p-7">
+        <div>
+          <img
+            className="mb-20"
+            src={testimonial.testimonials_img.data.attributes.url}
+            alt="Logo"
+            width={200}
+            height={40}
+          />
+          {/* <img src="/logo.png" alt="" /> */}
+        </div>
+        <div
+          dangerouslySetInnerHTML={{ __html: testimonial.testimonials_text }}
+          className="py-7 italic font-light"
+        />
+      </div>
+
+      <div
+        className={`${
+          index % 2 == 0 ? "bg-blue-medium" : "bg-green-dark"
+        } absolute bottom-0 left-0 right-0  p-10 rounded-bl-lg rounded-br-lg`}
+      >
+        <p className={`${index % 2 == 0 ? "text-white" : "text-black"}`}>
+          {testimonial.testimonials_clientname}
+        </p>
+        <span className={`${index % 2 == 0 ? "text-white" : "text-black"}`}>
+          {testimonial.testimonials_client_role}
+        </span>
+      </div>
+    </div>
+  );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,11 +7,12 @@ import Explore from "../../components/homepage/Explore";
 import StakeholdersOpenEcosystems from "../../components/homepage/StakeholdersOpenEcosystems";
 import ProductsServices from "../../components/homepage/ProductsServices";
 import Hero from "../../components/homepage/Hero";
+import Testimonials from "../../components/homepage/Testimonials";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home({ data }) {
-  console.log(data);
+
   const newData = data.attributes;
   return (
     <Layout>
@@ -28,6 +29,7 @@ export default function Home({ data }) {
         <StakeholdersOpenEcosystems videos={[data?.attributes.stakeholder_img_1, data?.attributes.stakeholder_img_2, data?.attributes.stakeholder_img_3]} title={data?.attributes?.stakeholder_title}/>
         <ProductsServices title={data?.attributes?.product_services_title} product_cards={data?.attributes?.product_cards}/>
         <Explore vertical_description_card={data?.attributes?.vertical_description_card} title={data?.attributes?.explore_sectors_title}/>
+        <Testimonials data={data.attributes.testimonials} title={newData.testimonials_title}/>
       </div>
     </Layout>
   );
