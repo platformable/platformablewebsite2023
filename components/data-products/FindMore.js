1522186400ec3ae5ec789fdf8132ea55061dd78b
Alexei Garban <garban.valdeon@gmail.com>
2023-07-05 12:30:50 +0200

    data products updates

--- components/data-products/FindMore.js
+++ components/data-products/FindMore.js
@@ -1,5 +1,58 @@
-export default function FindMore () {
-    return (
-        <section className="container mx-auto py-10">Find more</section>
-    );
-};
+import styles from "@/styles/DataProducts.module.css";
+
+export default function FindMore({ data }) {
+  return (
+    <section className={`py-10  ${styles["data-products-our-process-bg"]}`}>
+      <div className="container mx-auto">
+        <div className="flex gap-x-5 items-center  mb-10">
+          <img
+            src="/platformable-icon-white.svg"
+            alt=""
+            className="md:flex hidden"
+          />
+          <h2
+            className={`text-white  font-bold font-karla  md:leading-10 leading-8 `}
+          >
+            Find out more
+          </h2>
+        </div>
+        <div className="grid md:grid-cols-3 gap-x-5 md:px-0 px-5 gap-y-5">
+          <div className="find-more-cards bg-white rounded-md shadow-md p-7">
+            <h6 className="text-center text-[var(--blue)] font-medium">Understand</h6>
+            
+            <div className="flex justify-center my-10">
+                <img src="https://dummyimage.com/200x200/000/fff" alt="" />
+            </div>
+
+            <p className="text-[var(--purple-medium)] leading-8">
+              Read more about how Platformable builds data products and learn
+              about our sustainability/scalability assessment model
+            </p>
+          </div>
+
+          <div className="find-more-cards bg-white rounded-md shadow-md p-7">
+            <h6 className="text-center text-[var(--blue)] font-medium">Act</h6>
+           <div className="flex justify-center my-10">
+                <img src="https://dummyimage.com/200x200/000/fff" alt="" />
+            </div>
+
+            <p className="text-[var(--purple-medium)] leading-8">
+            Choose from some of the tools we use and recommend when building data products
+            </p>
+          </div>
+
+          <div className="find-more-cards bg-white rounded-md shadow-md p-7">
+            <h6 className="text-center text-[var(--blue)] font-medium">Engage</h6>
+           <div className="flex justify-center my-10">
+                <img src="https://dummyimage.com/200x200/000/fff" alt="" />
+            </div>
+
+            <p className="text-[var(--purple-medium)] leading-8">
+            Contact us form/calendly booking widget
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
--- components/homepage/Testimonials.js
+++ components/homepage/Testimonials.js
@@ -2,8 +2,8 @@ import React from "react";
 import TestimonialCard from "./TestimonialCard";
 import styles from "@/styles/Homepage.module.css";
 
-export default function Testimonials({ data, title }) {
-  const logos = [
+export default function Testimonials({ data, title, logos }) {
+  const brands = [
     {
       logo: "/LN_logo.png",
     },
@@ -27,7 +27,11 @@ export default function Testimonials({ data, title }) {
     <section className={`py-10  ${styles["testimonials-bg"]} `}>
       <div className="container mx-auto md:mb-10 mb-0">
         <div className="flex gap-x-5 items-center ">
-          <img src="/platformable-icon-purple.svg" alt="" className="md:flex hidden"/>
+          <img
+            src="/platformable-icon-purple.svg"
+            alt=""
+            className="md:flex hidden"
+          />
           <h2
             className={`text-[#5B24EC]  font-bold font-karla  md:leading-10 leading-8`}
           >
@@ -42,7 +46,7 @@ export default function Testimonials({ data, title }) {
             data.map((testimonial, index) => {
               return (
                 <div
-                  className="item bg-white grid grid-rows-[1fr_3fr_1fr] rounded-lg relative p-7 shadow-md"
+                  className="item bg-white grid md:grid-rows-[0.5fr_10fr_0.5fr] grid-rows-[0.5fr_10fr_1fr] rounded-lg relative p-7 shadow-md"
                   key={index}
                 >
                   <img
@@ -81,17 +85,18 @@ export default function Testimonials({ data, title }) {
               );
             })}
         </div>
-
-        <div className="grid gap-x-5 md:grid-cols-6 grid-cols-2 md:gap-y-0 gap-y-3 items-center mt-20 justify-center md:px-0 px-5">
-          {logos &&
-            logos.map((logo, index) => {
-              return (
-                <div key={index} className="flex justify-center">
-                  <img src={logo.logo} alt={"platformable"} />
-                </div>
-              );
-            })}
-        </div>
+        {logos && (
+          <div className="grid gap-x-5 md:grid-cols-6 grid-cols-2 md:gap-y-0 gap-y-3 items-center mt-20 justify-center md:px-0 px-5">
+            {brands &&
+              brands.map((logo, index) => {
+                return (
+                  <div key={index} className="flex justify-center">
+                    <img src={logo.logo} alt={"platformable"} />
+                  </div>
+                );
+              })}
+          </div>
+        )}
       </div>
     </section>
   );
