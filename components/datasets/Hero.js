5bb35d2b7c7bef0c92966235a078f7da22271ed1
leoncan122 <leon.can122@gmail.com>
2023-07-10 13:58:16 +0200

    Update Datasets page

--- components/datasets/Hero.js
+++ components/datasets/Hero.js
@@ -1,4 +1,4 @@
-import styles from "@/styles/DataProducts.module.css";
+import styles from "@/styles/Datasetspage.module.css";
 
 export default function Hero({ data }) {
   return (
@@ -7,27 +7,28 @@ export default function Hero({ data }) {
         <h1 className="mb-10 text-[var(--purple-medium)] font-bold">
           Datasets
         </h1>
-        <div
+        <p
           dangerouslySetInnerHTML={{
-            __html: '',
+            __html: 'Platformable offers a range of off-the-shelf datasets to help you track and understand key open ecosystems in banking/finance, health, government, privacy tech and the API technology industry. You can purchase these data sets or have them customised to meet your specific needs.',
           }}
           className="lg:w-4/6 xl:w-3/6"
         />
       </div>
       <div className={`${styles.hero_background_gradient} py-10 `}>
-        <div className="container mx-auto   flex gap-7">
-          <div
+        <div className="container mx-auto grid gap-7">
+        <img
+            src={'/hero_data_products.svg'}
+            alt="Data products mapped"
+            className="xl:-mt-96 lg:-mt-72 lg:-mr-14 hidden justify-self-end lg:block lg:w-2/6 xl:w-3/6"
+          />
+          <p
             dangerouslySetInnerHTML={{
-              __html: '',
+              __html: 'We can also work with you to design and build your own datasets. Using our data governance model, we help you map out the data model and our analysts can help you collect the data or reformat your existing data so that you have a dataset that can be used internally or as a marketable data product.',
             }}
             className="text-white "
           />
 
-          <img
-            // src={}
-            alt="Data products mapped"
-            className="xl:-mt-96 lg:-mt-72 lg:-mr-14 hidden lg:block lg:w-2/6 xl:w-3/6"
-          />
+         
         </div>
       </div>
     </section>
