c53c05c0e061e704b8bb78462629c1475429b3db
leoncan122 <leon.can122@gmail.com>
2023-07-11 18:01:02 +0200

    Add Api governance Hero

--- src/pages/products/api-governance.js
+++ src/pages/products/api-governance.js
@@ -5,12 +5,16 @@ import FindMore from "../../../components/data-products/FindMore";
 import Testimonials from "../../../components/homepage/Testimonials";
 import GovernanceServices from "../../../components/GovernanceServices";
 import GovernanceProducts from "../../../components/data-governance/GovernanceProducts";
+import Hero from "../../../components/api-governance/Hero";
+import OurProcess from "../../../components/api-governance/OurProcess";
 export default function APIGovernance({ data }) {
   console.log("data",data);
 
   return (
     <Layout>
+      <Hero data={data}/>
       <GovernanceProducts title={data?.products_title} products={data?.products} />
+      <OurProcess data={data}/>
       <GovernanceServices description={data?.services_description} services={data?.services}/>
       <Testimonials data={data?.testimonials} title={data?.testimonials_title} logos={false}/>
       <FindMore data={data}/>
@@ -22,7 +26,7 @@ export default function APIGovernance({ data }) {
 export async function getServerSideProps(ctx) {
   try {
     const res = await fetch(
-      `${process.env.NEXT_PUBLIC_SERVER_URL}/api/api-governance?populate[hero_img]=*&populate[services][populate][service_img]=*&&populate[products]=*&populate[testimonials][populate][testimonials_img]=*`
+      `${process.env.NEXT_PUBLIC_SERVER_URL}/api/api-governance?populate[hero_img]=*&populate[services][populate][service_img]=*&&populate[products][populate][bookbuybtn_img]=*&populate[testimonials][populate][testimonials_img]=*`
     );
     const data = await res.json();
 
