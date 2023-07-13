9898709a966d901d43fdf2ce86c5d49c3f0f8c7b
leoncan122 <leon.can122@gmail.com>
2023-07-13 11:57:08 +0200

    Add Engage page

--- components/api-governance/OurProcess.js
+++ components/api-governance/OurProcess.js
@@ -1,5 +1,4 @@
 import styles from "@/styles/APIGovernance.module.css";
-import { EngageDiagram } from "../EngageDiagram";
 
 export default function OurProcess({ data }) {
   return (
@@ -23,10 +22,9 @@ export default function OurProcess({ data }) {
           }}
           className="text-white"
         />
-        <EngageDiagram />
-        {/* <div className="mt-10 flex items-center justify-center bg-white bg-opacity-50 rounded py-72">
+        <div className="mt-10 flex items-center justify-center bg-white bg-opacity-50 rounded py-72">
           <h4 className="text-[var(--purple-dark)]">Diagram</h4>
-        </div> */}
+        </div>
       </div>
     </section>
   );
