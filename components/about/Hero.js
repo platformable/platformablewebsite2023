983dcaeaa37fe81ee905e62bdead6df8e688ee79
Alexei Garban <garban.valdeon@gmail.com>
2023-07-26 17:19:23 +0200

    team

--- components/about/Hero.js
+++ components/about/Hero.js
@@ -3,7 +3,7 @@ import styles from "@/styles/Aboutpage.module.css";
 export default function Hero({ data }) {
   return (
     <section className={`hero container mx-auto text-white`}>
-        <div className=" text-white flex items-center gap-7  py-10">
+        <div className=" text-white flex md:flex-nowrap flex-wrap items-center gap-7  py-10">
           <div
             dangerouslySetInnerHTML={{
               __html: data?.hero_description,
--- components/about/Team.js
+++ components/about/Team.js
@@ -1,7 +1,24 @@
-import React from 'react'
+import React from "react";
 
-export default function Team() {
+export default function Team({ data,styles }) {
+  
   return (
-    <div>Team</div>
-  )
+    <section className={`${styles.team_bg} pt-7 pb-24`}>
+    <div className="container mx-auto ">
+      <div id="header" className="flex gap-3 mb-10">
+          <img src="/logo_icon_white.png" alt="Platofrmable logo" />
+          <h4 className="font-bold text-white">Our Team</h4>
+        </div>
+      <div className="flex flex-wrap justify-evenly">
+        {data.map((member, index) => (
+          <div key={index} className="w-full md:w-1/4 px-4 ">
+            <div className="card">
+              <img src={member.attributes.url} alt="" key={index} />
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+    </section>
+  );
 }
