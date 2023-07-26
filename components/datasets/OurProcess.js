0037c7b43fae1452e19c7d8f0e5429484fc7eb4d
Alexei Garban <garban.valdeon@gmail.com>
2023-07-26 09:33:43 +0200

    our porcess

--- components/datasets/OurProcess.js
+++ components/datasets/OurProcess.js
@@ -1,23 +1,89 @@
 import styles from "@/styles/Datasetspage.module.css";
 
+const ProcessCards = ({data})=>{
+
+  return data?.map((process,index)=>{
+    return (
+      
+        <div className={`${process.is_subprocess ? 'bg-purple-light':styles['datasets-product-bg']} grid grid-cols-[0.5fr_3fr_1fr] rounded-lg p-7 items-center shadow`} key={index}>
+         <div><img src={process.platformable_icon} alt="" /></div>
+         <div className="text-white flex items-center ml-5"> {process.process}</div>
+         <div className="flex justify-end slef-start"><img src={process.product_icon} alt="" /></div>
+        </div>
+   
+        
+    )
+  })
+
+
+}
+
 export default function OurProcess({ data }) {
+
+
+const OurProcessData=[
+  {
+    process:`
+Design thinking:
+  
+User needs, Business Model Potential, Scalability assessment`,
+    platformable_icon:'/logo_icon_white.png',
+    product_icon:'/logo_icon_white.png'
+  },
+
+  {
+    process:`Collect data`,
+    platformable_icon:'/logo_icon_white.png',
+    product_icon:'/logo_icon_white.png'
+  },
+  {
+    process:`Data inventory, Data model design`,
+    platformable_icon:'/logo_icon_white.png',
+    product_icon:'/logo_icon_white.png'
+  },
+  {
+    process:`Automate maintenance and DataOps processes`,
+    platformable_icon:'/logo_icon_white.png',
+    product_icon:'/logo_icon_white.png'
+  },
+  {
+    process:`Data Governance process model`,
+    platformable_icon:'/logo_icon_white.png',
+    product_icon:'/logo_icon_white.png',
+    is_subprocess:true
+  },
+  {
+    process:`Build data model`,
+    platformable_icon:'/logo_icon_white.png',
+    product_icon:'/logo_icon_white.png'
+  },
+  {
+    process:`Socialize data for internal use and or assist with data product Marketing`,
+    platformable_icon:'/logo_icon_white.png',
+    product_icon:'/logo_icon_white.png'
+  },
+]
+
+
+
   return (
     <section className={`py-10  ${styles["datasets-our-process-bg"]}`}>
       <div className="container mx-auto">
         <div className="flex gap-x-5 items-center  mb-10">
           <img
-            src="/platformable-icon-white.svg"
+            src="/platformable-icon-purple-dark.png"
             alt=""
             className="md:flex hidden"
           />
           <h2
-            className={`text-white  font-bold md:leading-10 leading-8 `}
+            className={`text-[#3B27CE]  font-bold md:leading-10 leading-8 `}
           >
             Our Process
           </h2>
         </div>
-        <div className="flex items-center justify-center bg-white bg-opacity-50 rounded py-72">
-          <h4 className="text-[var(--purple-dark)]">Diagram</h4>
+        <div className="grid md:grid-cols-2 grid-cols-1 gap-5" >
+          
+          <ProcessCards data={OurProcessData}/>       
         </div>
       </div>
     </section>
