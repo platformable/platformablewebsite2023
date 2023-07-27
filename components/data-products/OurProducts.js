c2c8cd153064dd66dd9f98cea1014da85544dddb
leoncan122 <leon.can122@gmail.com>
2023-07-27 18:19:52 +0200

    Update Data-products page

--- components/data-products/OurProducts.js
+++ components/data-products/OurProducts.js
@@ -7,56 +7,99 @@ export default function OurProducts({ title, products }) {
         <img src="/platformable-icon-purple.svg" alt="Platformable logo" className=""/>
         <h3 className="text-[var(--purple-medium)] font-bold">{title}</h3>
       </div>
-      <div className="grid md:grid-cols-2 xl:grid-cols-4  gap-10">
-        {products?.map((card, i) => (
-          <div
-            className="grid grid-rows-[1fr_3fr] shadow-[0px_3px_6px_#86858B] rounded-md lg:rounded-xl"
-            data-card-number={i + 1}
-            key={i}
-          >
-            <div
-              className={`${styles.card_heading} flex items-center justify-center text-center rounded-t-md lg:rounded-t-xl p-7`}
-            >
-              <h6 className="text-white font-bold">
-                {card?.data_products_product_name}
-              </h6>
+      
+      <div className="grid xl:mt-10 gap-1">
+          <div className="grid grid-cols-[1.5fr_2fr_1fr_1fr] ">
+            <div className="flex items-center bg-[#3328C3] px-7 gap-5 rounded-tl-xl">
+              <div className="w-2/6 flex justify-center">
+                <img  src="/logo_icon_white.png" alt="Platformable logo" />
+              </div>
+              <h6 className="text-white font-bold">Data products</h6>
             </div>
-            <div className="grid place-content-center rounded-b-md lg:rounded-b-xl pb-10">
-              <div
-                dangerouslySetInnerHTML={{
-                  __html: card?.data_products_product_description,
-                }}
-                className="pt-5 pb-10 px-3 xl:px-5 text-[var(--purple-medium)] "
-              />
-
-              {card.data_products_product_price_quarter &&
-                card.data_products_product_price_year && (
-                  <div className="grid gap-3 items-center justify-center px-3 lg:px-5 2xl:px-10">
-                    <div className="flex items-center justify-center"><img src="/pdf_copy.svg" alt="pdf copy"/> </div>
-                    <p className="text-center px-5 text-[var(--purple-medium)]">Simple copy</p>
-                    <button className="rounded-md  bg-[var(--purple-light)] py-2 mb-16 text-white">
-                      <p>Download</p>
-                    </button>
-                    <div className="grid gap-5">
-                      <p className="text-center font-bold text-[var(--pink)]">
-                        <span className="text-[var(--blue-extra-dark)]">$150</span> per quarter
-                        <br />
-                        <span className="font-medium text-[var(--blue-extra-dark)]">or</span>
-                        <br />
-                        <span className="text-[var(--blue-extra-dark)]">$450</span> per year 
-                        <br />
-                        (save %25)
-                      </p>
-                      <button className="bg-[var(--blue-extra-dark)] py-2 rounded-md text-white">
-                        <p>Purchase</p>
-                      </button>
-                    </div>
-                  </div>
-                )}
+            <div className="grid place-items-center py-5 bg-white">
+              <img src="/overview.svg" alt="datapoints svg" />
+              <p className="font-bold text-[var(--purple-medium)]">Overview</p>
+            </div>
+            <div className="grid place-items-center py-5 px-7 bg-[#F6F4FF]">
+            <img src="/sample_icon.svg" alt="Specification sheet svg" />
+              <p className="font-bold text-center text-[var(--purple-medium)]">Sample copy</p>
+            </div>
+            <div className="grid place-items-center py-5 px-7 bg-[#E9E8F8]">
+              <img src="/purchase_icon.svg" alt="bulk download svg" />
+              <p className="font-bold text-[var(--purple-medium)]">Purchase</p>
             </div>
           </div>
-        ))}
-      </div>
+          {products?.map((card, i) => (
+            <div
+              className="grid grid-cols-[1.5fr_2fr_1fr_1fr] "
+              data-card-number={i + 1}
+              key={i}
+            >
+              <div
+                className={`${styles.card_heading} flex items-center justify-start gap-7 py-5  px-7`}
+              >
+                <img src={card?.product_icon?.data?.attributes?.url} alt="" className="w-2/6" />
+                <h6 className="text-white font-bold w-4/6">
+                  {card?.data_products_product_name}
+                </h6>
+              </div>
+            
+              <div className="flex items-center p-7 justify-center bg-white p">
+                
+                <div
+                    dangerouslySetInnerHTML={{
+                      __html: card?.data_products_product_description,
+                    }}
+                    className="text-[var(--purple-medium)]"
+                  />
+              </div>
+
+            
+              <div className="grid items-center px-7 bg-[#F6F4FF]">
+                <button className="rounded-md shadow bg-[var(--purple-light)] px-3 py-2 text-white">
+                  <p>Download</p>
+                </button>
+              </div>
+              
+              <div className="grid items-center px-7 bg-[#E9E8F8]">
+                {card.data_products_product_price_quarter &&
+                card.data_products_product_price_year ? (
+                  <div className="grid grid-rows-2 gap-y-10">
+                  <button className="bg-[var(--yellow)] px-3 py-2 shadow rounded-md text-[var(--purple)]">
+                    <p><strong>${card.data_products_product_price_quarter}</strong> per quarter</p>
+                  </button>
+                  <button className="bg-[var(--orange-medium)] leading-snug px-3 py-2 shadow rounded-md text-[var(--purple)]">
+                    <p><strong>${card.data_products_product_price_year}</strong> per year</p>
+                    <small>save 25%</small>
+                  </button>
+                  </div>
+                ) : (
+                  <button className="bg-[var(--pink)] shadow px-3 py-2 rounded-md text-white">
+                  <p>Join waiting list</p>
+                </button>
+                )}
+                
+              </div>
+              
+              {/* <div className="grid items-center px-7 bg-[#DEDCF5]">
+                <button className="bg-[var(--pink)] shadow px-3 py-2 rounded-md text-white">
+                  <p>Join waiting list</p>
+                </button>
+              </div> */}
+            </div>
+          ))}
+           {/* <div className="grid grid-cols-[1.5fr_1fr_1fr_1fr_1fr] bg-white py-7">
+            <div className="px-7 col-start-1 col-end-5 text-[#3328C3] ">
+             <h5 className="mb-1 font-bold">{"Didn't find the product you were looking for ?"}</h5>
+             <p>If you have a specific product in mind or if you need assistance in building your own dataset</p>
+            </div>
+            <div className="grid items-center px-7 ">
+                <button className="bg-[var(--yellow)] px-3 py-2 rounded-md text-[var(--purple)]">
+                  <p>Buy now</p>
+                </button>
+              </div>
+          </div> */}
+        </div>
     </section>
   );
 }
