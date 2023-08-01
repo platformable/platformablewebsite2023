4b61d94a43d52f944b02b6675b1a68a68845176c
leoncan122 <leon.can122@gmail.com>
2023-08-01 12:09:35 +0200

    Update Hero

--- components/speaker-engagement/Hero.js
+++ components/speaker-engagement/Hero.js
@@ -5,18 +5,18 @@ export default function Hero({ data }) {
     <section className="">
       <div className="container  mx-auto   py-10">
         <h1 className="mb-10 text-[var(--purple-medium)] font-bold">
-          {data?.data_products_hero_title}
+          {data?.title}
         </h1>
         <div className="flex">
           <div
             dangerouslySetInnerHTML={{
               __html:
-                "Platformable team members are available for speaking engagements, conference moderation, roundtable and workshop facilitation and online presentations. We can speak on a range of topics, drawing on our extensive library of previous talks, or can tailor-make a presentation or keynote to your needs.",
+                data?.hero_description,
             }}
             className="lg:w-3/6 xl:w-[60%] 2xl:w-3/5"
           />
           <img
-            src={data?.data_products_hero_img?.data.attributes.url}
+            src={data?.hero_img?.data.attributes.url}
             alt="Data products mapped"
             className="lg:-mb-[2rem] xl:-mt-24 lg:-mr-10 hidden justify-self-end lg:block lg:w-3/6 xl:w-[40%] 2xl:w-2/5 h-96"
           />
