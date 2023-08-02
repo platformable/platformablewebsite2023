e858a0a8d103bc395295e680a9609f93f7035775
leoncan122 <leon.can122@gmail.com>
2023-08-02 13:02:49 +0200

    Update OurSolutions compo

--- components/open-ecosystem-consultancy/OurSolutions.js
+++ components/open-ecosystem-consultancy/OurSolutions.js
@@ -15,23 +15,23 @@ export default function OurSolutions({ data }) {
           </h3>
         </div>
         <div className="grid mt-5 xl:mt-10 gap-y-4 lg:gap-y-7">
-          
-          <div className="grid lg:grid-cols-[1fr_8.5fr] ">
-            <div
-              className={`${styles.bg_solution_tile} flex items-center justify-center bg-[#3328C3] max-lg:py-5 px-3 xl:px-7 gap-5 rounded-l-xl`}
-            >
-              <img src="/logo_icon_white.png" alt="Platformable logo" />
-            </div>
-            <div className="grid place-items-center px-7 py-5 bg-white rounded-r-xl">
+          {data?.solutions.map((solution) => (
+            <div className="grid lg:grid-cols-[1fr_8.5fr] lg:h-48 xl:h-32">
               <div
-            dangerouslySetInnerHTML={{
-              __html:
-                data?.description,
-            }}
-            className="lg:w-3/6 xl:w-[60%] 2xl:w-3/5"
-          />
+                className={`${styles.bg_solution_tile} flex items-center justify-center bg-[#3328C3] max-lg:py-5 px-3 xl:px-7 gap-5  rounded-t-xl lg:rounded-l-xl lg:rounded-tr-none  `}
+              >
+                <img src={solution?.icon?.data?.attributes.url} alt="Platformable logo" />
+              </div>
+              <div className="grid place-items-center px-7 py-5 bg-white rounded-b-xl lg:rounded-l-none lg:rounded-r-xl">
+                <div
+                  dangerouslySetInnerHTML={{
+                    __html: solution?.description,
+                  }}
+                  className=""
+                />
+              </div>
             </div>
-          </div>
+          ))}
 
           <div className="grid grid-cols-[2fr_8.5fr_2fr] bg-white py-7">
             <div className="px-7 col-start-1 col-end-3 text-[#3328C3]  flex items-center gap-x-5 xl:gap-x-16">
