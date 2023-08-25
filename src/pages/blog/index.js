9bc7dd20aef4d3ef445961de91ae6dab88c2b8dd
leoncan122 <leon.can122@gmail.com>
2023-08-25 12:41:12 +0200

    Add MEta to all pages and queries

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,10 +1,12 @@
 import Layout from "../../../components/Layout";
 import styles from "../../styles/Blog.module.css";
 import ResourcesSearch from "../../../components/blog/ResourcesSearch";
+import Meta from "../../../components/Meta";
 
 export default function BlogsIndex({ posts }) {
   return (
     <Layout>
+      <Meta title="Platformable Blog" />
       <section id="blog" className={`${styles["background_blog"]} py-10`}>
         <div className="container mx-auto">
           <h1 className="mb-5 font-bold text-white ">Blog</h1>
