824746e030983d55bcbc493bb88fc56bc58f0a2a
leoncan122 <leon.can122@gmail.com>
2023-10-17 12:19:01 +0200

    Add carousel

--- components/obof-trends-reports/Hero.js
+++ components/obof-trends-reports/Hero.js
@@ -1,11 +1,33 @@
-import styles from '@/styles/OBOFTrendsReportspage.module.css'
+import styles from "@/styles/OBOFTrendsReportspage.module.css";
 
 export default function Hero() {
-    return (
-        <div>
-            <h1>
-                Server Error
-            </h1>
+  return (
+    <div className="overflow-hidden">
+      <div className={styles.items}>
+        <div className={`${styles.item} ${styles.active}`}>
+          <img src="http://via.placeholder.com/500x500" alt="" />
         </div>
-    );
+        <div className={`${styles.item} ${styles.next}`}>
+          <img src="http://via.placeholder.com/500x500" alt="" />
+        </div>
+        <div className={`${styles.item}`}>
+          <img src="http://via.placeholder.com/500x500" alt="" />
+        </div>
+        <div className={`${styles.item}`}>
+          <img src="http://via.placeholder.com/500x500" alt="" />
+        </div>
+        <div className={`${styles.item} ${styles.prev}`}>
+          <img src="http://via.placeholder.com/500x500" alt="" />
+        </div>
+        <div className={`${styles.buttonContainer}`}>
+          <div className={styles.button}>
+            <i className="fas fa-angle-left"></i>
+          </div>
+          <div className={styles.button}>
+            <i className="fas fa-angle-right"></i>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
---
+++ src/pages/products/data-products/index.js
@@ -0,0 +1,46 @@
+import Layout from "../../../../components/Layout";
+import Hero from "../../../../components/data-products/Hero";
+import styles from '@/styles/DataProducts.module.css'
+import OurProducts from "../../../../components/data-products/OurProducts";
+import OurProcess from "../../../../components/data-products/OurProcess";
+import FindMore from "../../../../components/data-products/FindMore";
+import Testimonials from "../../../../components/homepage/Testimonials";
+import Meta from "../../../../components/Meta";
+import OurProductssMobile from "../../../../components/data-products/OurProductsMobile";
+export default function DataProducts({ data }) {
+  // console.log("data",data);
+
+  return (
+    <Layout>
+      <Meta title={data?.data_products_hero_title} data={data} />
+      <Hero data={data}/>
+      <OurProducts products={data?.products} title={data?.data_products_products_titl}/>
+      <OurProductssMobile products={data?.products} title={data?.data_products_products_titl}/>
+      <OurProcess data={data}/>
+      <Testimonials data={data?.testimonials} title={data?.data_product_testimonial_title} logos={false}/>
+      <FindMore sectors={data?.sectors}/>
+
+    </Layout>
+  );
+}
+
+export async function getServerSideProps(ctx) {
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_SERVER_URL}/api/data-product?populate[data_products_hero_img]=*&populate[featured_img]=*&populate[products][populate][product_icon]=*&populate[testimonials][populate][testimonials_img]=*&populate[discovery_images]=*&populate[scope_img1]=*&populate[scope_img2]=*&populate[scope_img3]=*&populate[sectors][populate][icon]=*&&populate[sectors][populate][image]=*`
+    );
+    const data = await res.json();
+
+    return {
+      props: {
+        data: data?.data?.attributes,
+      },
+    };
+  } catch (error) {
+    return {
+      props: {
+        data: "No Data",
+      },
+    };
+  }
+}
