8be54382bdc06977f8b5d09600f34e977cff3788
leoncan122 <leon.can122@gmail.com>
2023-10-18 13:12:12 +0200

    Add strapi variables

--- components/obof-trends-reports/ActionableInformation.js
+++ components/obof-trends-reports/ActionableInformation.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function ActionableInformation() {
+export default function ActionableInformation({data}) {
   return (
     <div className="container mx-auto">
       <div className="flex gap-x-5 items-center   py-10">
@@ -10,7 +10,7 @@ export default function ActionableInformation() {
           className="md:flex hidden"
         />
         <h3 className={`text-white  font-bold md:leading-10 leading-8 `}>
-          Actionable information on:
+          {data?.actionable_information_title}
         </h3>
       </div>
 
@@ -18,21 +18,24 @@ export default function ActionableInformation() {
         id="actionable-cards-container"
         className="actionable-cards-container grid lg:grid-cols-4 md:grid-cols-2 gap-x-5 grid-cols-1 gap-y-5"
       >
-        <div
+        {data?.actionable_information_card?.map((card,index)=> (
+          <div
+          key={index}
           id="actionable-card"
           className="actionable-card bg-[#5A23EC] p-7 grid grid-rows-[0.5fr_1.5fr_1fr] gap-y-5 rounded-lg"
         >
-          <p className="text-white font-bold text-center ">Insights</p>
+          <p className="text-white font-bold text-center ">{card?.title}</p>
 
           <div className="text-center flex justify-center">
-            <img src="/temporary_icon.svg" alt="" className="text-center" />
+            <img src={card?.image?.data?.attributes?.url} alt="" className="text-center" />
           </div>
 
           <p className="leading-6 text-white text-center">
-            Dive into key trends shaping the future of finance
+            {card?.description}
           </p>
         </div>
-        <div
+        ))}
+        {/* <div
           id="actionable-card"
           className="actionable-card bg-[#5A23EC] p-7 grid grid-rows-[0.5fr_1.5fr_1fr] gap-y-5 rounded-lg"
         >
@@ -73,7 +76,7 @@ export default function ActionableInformation() {
           <p className="leading-6 text-white text-center">
           Gain inspiration from real-world success stories
           </p>
-        </div>
+        </div> */}
       </div>
     </div>
   );
