b2d4067b02aaeee3a7639bbf7252840a5e4c7228
Alexei Garban <garban.valdeon@gmail.com>
2023-10-19 19:10:29 +0200

    new route for product pages

---
+++ src/pages/products/data-products/[slug]/index.js
@@ -0,0 +1,50 @@
+import Layout from "../../../../../components/Layout";
+import ActionableInformation from "../../../../../components/obof-trends-reports/ActionableInformation";
+import DiscoverBenefits from "../../../../../components/obof-trends-reports/DiscoverBenefits";
+import Hero from "../../../../../components/obof-trends-reports/Hero";
+import ProductDescription from "../../../../../components/obof-trends-reports/ProductDescription";
+import ProductDescriptionMobile from "../../../../../components/obof-trends-reports/ProductDescriptionMobile";
+import FindMore from "../../../../../components/data-products/FindMore";
+
+
+export default function OpenBankingOpenFinanceTrendsReports({data}) {
+    console.log(data)
+    return (
+        <>
+        <Layout>
+            <Hero data={data}/>
+
+            <section id="actionable-discover" className="bg--gradient-obof py-10">
+                <ActionableInformation data={data}/>
+                <DiscoverBenefits data={data}/>
+            </section>
+            <ProductDescription data={data}/>
+            <ProductDescriptionMobile products={data?.products}/>
+            <FindMore sectors={data?.sectors}/>
+        </Layout>
+        </>
+    );
+}
+
+export async function getServerSideProps(ctx) {
+    const slug=ctx.params.slug
+    
+    try {
+      const res = await fetch(
+        `${process.env.NEXT_PUBLIC_SERVER_URL}/api/product-landing-pages?filters[slug]=${slug}&populate[hero_images]=*&populate[actionable_information_card][populate][image]=*&populate[discover][populate][persona][populate][persona_image]=*&populate[products][populate][product_icon]=*&populate[sectors][populate][icon]=*&populate[sectors][populate][image]=*&populate[discover][populate][benefits]=*`
+      );
+      const data = await res.json()
+  
+      return {
+        props: {
+          data: data?.data[0].attributes,
+        },
+      };
+    } catch (error) {
+      return {
+        props: {
+          data: "No Data",
+        },
+      };
+    }
+  }
--- src/pages/products/data-products/index.js
+++ src/pages/products/data-products/index.js
@@ -25,9 +25,11 @@ export default function DataProducts({ data }) {
 }
 
 export async function getServerSideProps(ctx) {
+
   try {
     const res = await fetch(
       `${process.env.NEXT_PUBLIC_SERVER_URL}/api/data-product?populate[data_products_hero_img]=*&populate[featured_img]=*&populate[products][populate][product_icon]=*&populate[testimonials][populate][testimonials_img]=*&populate[discovery_images]=*&populate[scope_img1]=*&populate[scope_img2]=*&populate[scope_img3]=*&populate[sectors][populate][icon]=*&&populate[sectors][populate][image]=*`
+     
     );
     const data = await res.json();
 
