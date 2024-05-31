592cb74c99533ffd17820235c9fdf5a98c372b21
leoncan122 <leon.can122@gmail.com>
2024-05-31 14:51:13 +0200

    Add style for new hero section

--- components/prooduct/op/Hero.js
+++ components/prooduct/op/Hero.js
@@ -15,7 +15,7 @@ const [primaryCallToAcctionBgColor,setPrimaryCallToAcctionBgColor]=useState('')
   },[data])
   
   return (
-    <section className="overflow-hidden bg-gradient-to-b from-[#A17EFF20] to-[#F0EBFD10] to-30%  text-center pt-20 pb-10">
+    <section className="overflow-hidden bg-gradient-to-b from-[#A17EFF20] from-30%  via-[#E7DFFD20] via-50% to-[#F0EBFD20] to-50% text-center pt-20 pb-10">
       <div className="container mx-auto">
         <h1 className={`text-lg mb-3  font-bold`} style={{color:data.hero.headingColor}}>{data?.hero?.SeoKeyword}</h1>
         <h2 className="font-bold mb-5" style={{color:data?.hero?.BigHeadlineTextColor}}>
@@ -24,6 +24,10 @@ const [primaryCallToAcctionBgColor,setPrimaryCallToAcctionBgColor]=useState('')
         <h6>
         {data?.hero_description}
         </h6>
+        <div id="guarantee-hero-container" className="py-7 mx-32 bg-gradient-to-r from-trasnparent via-zinc-600 to-transparent">
+          <span>Trusted by Innovators</span>
+          <p>Cancel at anymtine, no strings attached</p>
+        </div>
         <div className="flex flex-col md:flex-row gap-5 justify-center mt-4 mb-10">
           <Link href={"/"} >
           <button className={` px-3 py-2 shadow rounded-md text-white w-56`} style={{backgroundColor:data.hero.primaryCallToActionBgColor}}>
