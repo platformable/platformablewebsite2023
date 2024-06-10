212b94c485a2462f36ec65b0b4f2454eff947a8f
leoncan122 <leon.can122@gmail.com>
2024-06-10 13:15:22 +0200

    add blog metadesc

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -4,10 +4,13 @@ import ResourcesSearch from "../../../components/blog/ResourcesSearch";
 import Meta from "../../../components/Meta";
 
 export default function BlogsIndex({ posts }) {
-  console.log("posts", posts)
+  // console.log("posts", posts)
+  const metaData = {
+      excerpt: 'Stay updated with the latest trends and insights on open data, APIs, and ecosystem innovation.'
+  }
   return (
     <Layout>
-      <Meta title="Platformable Blog" />
+      <Meta title="Platformable Blog" data={metaData}/>
       <section id="blog" className={`${styles["background_blog"]} py-10`}>
         <div className="container mx-auto">
           <h1 className="mb-5 font-bold text-white ">Blog</h1>
