3a92a15897147e6eb80de10fc900bc5001337c0a
Alexei Garban <garban.valdeon@gmail.com>
2024-07-02 16:25:59 +0200

    fix bullets

--- components/home/Collaborators.js
+++ components/home/Collaborators.js
@@ -4,7 +4,7 @@ export default function Collaborators({data}) {
         <section className="bg-white">
                 <div className="container mx-auto py-6 lg:py-10">
                     <h4 className="text-xl font-bold text-[#FD27FF] text-center">{data?.collaborators?.heading}</h4>
-                    <div className="grid grid-cols-2 items-center justify-between gap-3 lg:gap-3 lg:grid-cols-6 mt-5 lg:mt-8">
+                    <div className="grid grid-cols-2 items-center justify-between  lg:gap-0 lg:grid-cols-7 mt-5 lg:mt-8">
                         {data?.collaborators?.collaboratorLogo?.map((img, index) => (
                             <center key={index}><img  src={img.collaborator_img?.data?.attributes?.url} alt={'logo'} className="object-fill p-0 m-0" width={140} height={300}/></center>
                         ))}
