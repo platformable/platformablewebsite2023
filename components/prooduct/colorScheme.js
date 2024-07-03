9baa8e3f75b00a4292e4322d9159437db7a37810
leoncan122 <leon.can122@gmail.com>
2024-07-03 12:34:26 +0200

    Add color scheme other sectors

--- components/prooduct/colorScheme.js
+++ components/prooduct/colorScheme.js
@@ -53,5 +53,115 @@ export const colorSchemeBySector = {
             secondaryCallToActionTextColor: '#5E2AED',
             secondaryCallToActionBorderColor: '#5E2AED'
         }
+    },
+    'OH': {
+        hero: {
+            backgroundColor: 'linear-gradient(to top, #7D44FF 0%, #492ED8 70%)',
+            headingColor: '#fff',
+            bigHeadlineTextColor: '#fff',
+            subHeadlineTextColor: '#fff',
+            primaryCallToActionBgColor: '#1D61FE',
+            primaryCallToActionTextColor: '#fff',
+            primaryCallToActionBorderColor: 'transparent',
+            secondaryCallToActionBgColor: 'transparent',
+            secondaryCallToActionTextColor: '#fff',
+            secondaryCallToActionBorderColor: '#fff',
+            socialSectionBackgroundColor: 'linear-gradient(90deg, #5031DE 0%, #1217A2EE 55%, #5031DE 100%)',
+            socialHeadingTextColor: '#00BAFF',
+            socialSubheadingTextColor: '#fff',
+        },
+        testimonials: {
+            testimonialSectionBgColor: '#DCE9FF',
+            testimonialsCardBorderColor: '#5E2AED',
+            nameColor: '#1E1D4C',
+            positionColor: '#1E1D4C',
+
+        },
+        whatsIncluded: {
+            backgroundColor: '#fff',
+            companyNameColor: '#3425C9',
+            whatsIncludedMainColor: '#7D43FF',
+            whatsIncludedTextColor: '#000000',
+            whatsIncludedItemTitleColor: '#1E60FD',
+        },
+        benefits: {
+            backgroundColor: 'linear-gradient(to top, #1D61FE 0%, #6033E2 100%)',
+            benefitshHeadingColor: '#fff',
+            boxLabelBackgroundColor: '#fff',
+            boxLabelTextColor: '#5E2AED'
+        },
+        personas: {
+            backgroundColor: '#fff',
+            mainColor: '#5E2AED',
+            textColor: '#000000'
+        },
+        pricing: {
+            backgroundColor: 'linear-gradient(to right, #6A39EE 0%, #2D16A5 100%)',
+            headingTextColor: '#00BAFF',
+            subheadingTextColor: '#fff',
+            descriptionTextColor: '#fff',
+            mainColor: '#6A39EE',
+            primaryCallToActionBgColor: '#1D61FE',
+            primaryCallToActionTextColor: '#fff',
+            primaryCallToActionBorderColor: 'transparent',
+            secondaryCallToActionBgColor: 'transparent',
+            secondaryCallToActionTextColor: '#5E2AED',
+            secondaryCallToActionBorderColor: '#5E2AED'
+        }
+    },
+    'OE': {
+        hero: {
+            backgroundColor: 'linear-gradient(to top, #7D44FF 0%, #492ED8 70%)',
+            headingColor: '#fff',
+            bigHeadlineTextColor: '#fff',
+            subHeadlineTextColor: '#fff',
+            primaryCallToActionBgColor: '#22DA8F',
+            primaryCallToActionTextColor: '#fff',
+            primaryCallToActionBorderColor: 'transparent',
+            secondaryCallToActionBgColor: 'transparent',
+            secondaryCallToActionTextColor: '#fff',
+            secondaryCallToActionBorderColor: '#fff',
+            socialSectionBackgroundColor: 'linear-gradient(90deg, #5031DE 0%, #1217A2EE 55%, #5031DE 100%)',
+            socialHeadingTextColor: '#22DA8F',
+            socialSubheadingTextColor: '#fff',
+        },
+        testimonials: {
+            testimonialSectionBgColor: '#CFFFEF',
+            testimonialsCardBorderColor: '#5E2AED',
+            nameColor: '#1E1D4C',
+            positionColor: '#1E1D4C',
+
+        },
+        whatsIncluded: {
+            backgroundColor: '#fff',
+            companyNameColor: '#3425C9',
+            whatsIncludedMainColor: '#7D43FF',
+            whatsIncludedTextColor: '#000000',
+            whatsIncludedItemTitleColor: '#00B96D',
+        },
+        benefits: {
+            backgroundColor: 'linear-gradient(to top, #22DA8F 0%, #6033E2 100%)',
+            benefitshHeadingColor: '#fff',
+            boxLabelBackgroundColor: '#fff',
+            boxLabelTextColor: '#5E2AED'
+        },
+        personas: {
+            backgroundColor: '#fff',
+            mainColor: '#5E2AED',
+            textColor: '#000000'
+        },
+        pricing: {
+            backgroundColor: 'linear-gradient(to right, #6A39EE 0%, #2D16A5 100%)',
+            headingTextColor: '#22DA8F',
+            subheadingTextColor: '#fff',
+            descriptionTextColor: '#fff',
+            mainColor: '#6A39EE',
+            primaryCallToActionBgColor: '#22DA8F',
+            primaryCallToActionTextColor: '#fff',
+            primaryCallToActionBorderColor: 'transparent',
+            secondaryCallToActionBgColor: 'transparent',
+            secondaryCallToActionTextColor: '#5E2AED',
+            secondaryCallToActionBorderColor: '#5E2AED'
+        }
     }
 }
