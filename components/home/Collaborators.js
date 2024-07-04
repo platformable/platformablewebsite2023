72721b9cdb08b4d3fb70b43a6369d8e3c2c7609e
leoncan122 <leon.can122@gmail.com>
2024-07-04 10:36:04 +0200

    test collaborator carousel

--- components/home/Collaborators.js
+++ components/home/Collaborators.js
@@ -1,5 +1,7 @@
 import Image from "next/image";
 export default function Collaborators({data}) {
+    const gapWidth = parseInt(getComputedStyle(document.documentElement).getPropertyValue('--purple'));
+    console.log(gapWidth)
     return (
         <section className="bg-white">
                 <div className="container mx-auto py-6 lg:py-10">
@@ -10,6 +12,30 @@ export default function Collaborators({data}) {
                         ))}
                     </div>
                 </div>
+
+                <div class="carousel" aria-live="polite" aria-label="Image Carousel" className="overflow-hidden w-full">
+                    <div class="carousel-track" className="flex">
+                        {slidesData.map((slide, index) => (
+                            <>
+                            <div className="bg-red-500/60">
+
+                            </div>
+                            </>
+                        ))}
+                    </div>
+                </div>
             </section>
     );
 }
+
+const slidesData = [
+    { src: './images/img1.jpg', description: 'Description 1' },
+    { src: './images/img2.jpg', description: 'Description 2' },
+    { src: './images/img3.jpg', description: 'Description 3' },
+    { src: './images/img4.jpg', description: 'Description 4' },
+    { src: './images/img5.jpg', description: 'Description 5' },
+    { src: './images/img6.jpg', description: 'Description 6' },
+    { src: './images/img7.jpg', description: 'Description 7' },
+    { src: './images/img8.jpg', description: 'Description 8' },
+    { src: './images/img9.jpg', description: 'Description 9' }
+  ];
