1f548563c6d92b4a0a320e305efbb8d8d64d3a84
Alexei Garban <garban.valdeon@gmail.com>
2024-07-12 13:22:32 +0200

    about

--- components/about/Hero.js
+++ components/about/Hero.js
@@ -4,12 +4,13 @@ export default function Hero({ data }) {
   return (
     <section className={`hero container mx-auto text-white`}>
         <div className=" text-white flex md:flex-nowrap flex-wrap items-center gap-7  py-10">
-          <div
+       {/*    <div
             dangerouslySetInnerHTML={{
-              __html: data?.hero_description,
+              __html: data?.hero_description
             }}
-            // className="lg:w-3/6 2xl:w-4/7"
-          />
+            className="text-[10px] leading-tight"
+          /> */}
+          <h1 className="md:text-3xl text-xl">{data?.hero_description.replace('<h1>','').replace('</h1>','')}</h1>
           <img src={data?.hero_img?.data?.attributes.url} alt="Platformable Environmnet logo" />
         </div>
 
