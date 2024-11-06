dedd8c85cbd9c38e96144959d3de97551bd3cc2f
leoncan122 <leon.can122@gmail.com>
2024-11-06 13:21:39 +0100

    Update middleware

--- src/middleware.js
+++ src/middleware.js
@@ -2,12 +2,18 @@ import { NextResponse } from 'next/server'
 import blogLinksSource from '/public/old_blog_links'
 
 export function middleware(request) {
+
   const pathname = request.nextUrl.pathname
 
-  const redirectData = pathname !== '/' ? blogLinksSource.find(item => item.old_link.includes(pathname)) : null
+  let redirectData;
+  // console.log("pathname",pathname)
 
-    // console.log(pathname)
-  if (redirectData) {
+  if(pathname !== '/') {
+    redirectData = blogLinksSource.find(item => item.old_link.includes(pathname)) || null
+  }
+
+  if(redirectData) {
+    // console.log('hay redirect data')
     const redirectEntry = redirectData.new_link === '/' ? request.nextUrl.origin : redirectData.new_link 
     const statusCode = redirectEntry.permanent ? 308 : 307
     return NextResponse.redirect(redirectEntry, statusCode)
@@ -15,4 +21,24 @@ export function middleware(request) {
  
   // No redirect found, continue without redirecting
   return NextResponse.next()
+}
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    {
+      source: '/((?!api|_next/static|_next/image|favicon.ico|js/).*)',
+      missing: [
+        { type: 'header', key: 'next-router-prefetch' },
+        { type: 'header', key: 'purpose', value: 'prefetch' },
+        { type: 'header', key: 'sec-fetch-site', value: 'same-origin' },
+      ],
+    },
+  ],
 }
