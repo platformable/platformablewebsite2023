8d2124a6684dba0daefd4517093e7f1e1c1b0e7a
leoncan122 <leon.can122@gmail.com>
2024-12-13 16:15:22 +0100

    Add blog canonical

--- components/homepage/Hero.js
+++ components/homepage/Hero.js
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import React, {useEffect, useRef, useState} from "react";
+import React, {useCallback, useEffect, useRef, useState} from "react";
 import styles from "@/styles/Homepage.module.css";
 import Image from 'next/image'
 
@@ -16,14 +16,14 @@ useEffect(() => {
   
   setIsMobile(isMobileDevice)
 }, [])
-  const handleFullScreen = () => {
+  const handleFullScreen = useCallback(() => {
       videoEl &&
         videoEl.current &&
         videoEl.current.requestFullscreen()
         .catch((error) => {
           setError('can´t reproduce video')
         });
-    };
+    }, [videoEl]);
     
   const subtitle = heroSubtitle?.split(" ")[0];
   const simpleSubtitle = heroSubtitle?.substr(heroSubtitle.indexOf(" ") + 1);
